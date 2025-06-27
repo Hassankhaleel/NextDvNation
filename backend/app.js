8d6480c70b35bbcf76b5f918ff17bc9a1c38f593
CodeWithZain10 <zainzahid10116@gmail.com>
2025-06-28 02:14:52 +0500

    setup db

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from './utils/db.js';
+
+connectDB();
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
