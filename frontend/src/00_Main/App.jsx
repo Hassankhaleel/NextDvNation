aae5c3845d763234348546202ff0d86b05d84dd3
Hassankhaleel <hk450408@gmail.com>
2025-06-29 01:28:11 +0500

    Added navbar nad her0 section

--- frontend/src/00_Main/App.jsx
+++ frontend/src/00_Main/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Hero from '../02-Sections/Hero/Hero'
 import Navbar from '../01_Components/Navbar/Navbar'
-
+import hero_img from '../02-Sections/Hero/bg_hero.png'
 function App() {
     return (
         <>
-            asd
             <Navbar />
         </>
     )
--- frontend/src/03_Paginations/Routers/Router.jsx
+++ frontend/src/03_Paginations/Routers/Router.jsx
@@ -1,13 +1,20 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router";
 import App from "../../00_Main/App";
+import Hero from "../../02-Sections/Hero/Hero";
+import Navbar from "../../01_Components/Navbar/Navbar";
+import Events from "../../02-Sections/Events/Events";
 
 const routing = createBrowserRouter(
 
     createRoutesFromElements(
-        <Route path="/" element={<App />}>
 
+        <Route path="/" element={<Navbar />}>
+            <Route index="true" element={<Hero />} />
+            <Route path="/events" element={<Events />} />
 
         </Route>
+
+
 
     )
 )
